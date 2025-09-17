16c51ab4005d30be640a57fcc51dfc88636144ef
VigneshRamesh01 <hyperianschythe@gmail.com>
2025-09-17 14:45:49 +0530

    Added a lombok class to check the getters and setters implementation

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,11 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+    @PostConstruct
+	public void init(){
+		TestLombok test = new TestLombok();
+		test.setId(1L);
+		test.setName("Test");
+		System.out.println("-----Results-------- : \n" + test.getId() +"\t"+test.getName()+"\n" + test.toString());
+	}
 }
